feaa9c63c6dc2c44162824254dacf1fdb3041785
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-04-15 09:47:56 -0600

    added User table to database, working on user show page

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name { "Tom Hanks" }
+    email { "whereyou@willson.com" }
+    # google_token { "tomhanks1234" }
+    # google_client_id { "google_client" }
+  end
+end
---
+++ spec/feature/user_can_see_visit_show_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "user can visit user show page" do 
+    it "can see category/difficulty level buttons" do
+        user = create(:user)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        # binding.pry
+        visit user_path(user)
+
+    end  
+    
+    it "can see stats section" do 
+
+    end 
+end 
