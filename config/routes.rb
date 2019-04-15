14cf9634f89447625bf230d507efc5937355f8a4
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-04-15 16:10:37 -0600

    added welcome page with login/register buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: "welcome#index"
+
+  get '/register', to: redirect('/auth/google_oauth2'), as: 'register'
+
+  
   resources :users, only: [:show]
 
 end
---
+++ spec/feature/visitor_can_register_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe 'Visitor Registeration' do
+  it 'vistitor can register using google Oath' do
+    visit "/"
+    click_link("Login/Register")
+  end
+end
